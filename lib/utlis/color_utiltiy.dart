3435afac7cf7212552e81f7bbfe42243b577f7b4
dalia <Daliazaitoun1@gmail.com>
2024-07-31 00:50:02 +0300

    colorUtlitiy

---
+++ lib/utlis/color_utiltiy.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorUtility{
+  static const scaffoldBackground = Color(0xfffcfcfc);
+
+  static const deepYellow = Color(0xffF7BA34);
+  static const lightYellow = Color(0xffF8C657);
+  static const semiYellow = Color(0xffF6C354);
+  static const mediumBlack = Color(0xff1D1B20);
+  static const main = Color(0xff477B72);
+  static const grey = Color(0xff858383);
+
+}
